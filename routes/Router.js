9f65696b314c23c38173637288351d0d4667e4fc
savi256 <isaacchidera256@gmail.com>
2022-08-15 17:51:08 -0700

    make it beta

--- routes/Router.js
+++ routes/Router.js
@@ -3,12 +3,12 @@ const express = require("express");
 const router = express.Router();
 
 const Tododocument = require("../controller/List");
-const Signed = require("../controller/Signup");
+// const Signed = require("../controller/Signup");
 const sentreviewsController = require("../controller/sentreviews");
 
 
-router.post("/create-signupdetails",Signed.Sign);
-router.get("/getlogin",Signed.findUser)
+// router.post("/create-signupdetails",Signed.Sign);
+// router.get("/getlogin",Signed.findUser)
 router.post("/create-list", Tododocument.createTododocument);
 router.get("/get-list", Tododocument.getTodolist);
 router.get("/getSpecific-list/:id", Tododocument.getspecificlist);
