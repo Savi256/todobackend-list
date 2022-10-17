d630abd3aee1336a54f145e467f80f90a466a5b6
savi256 <isaacchidera256@gmail.com>
2022-10-17 10:06:18 -0700

    better

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ app.use(express.json());
 app.use(
   cors({
     // origin: "http://localhost:3000",
-    origin: "https://tapp-todo-list.herokuapp.com",
-    // origin: "*",
+    // origin: "https://tapp-todo-list.herokuapp.com",
+    origin: "*",
   })
 );
 
